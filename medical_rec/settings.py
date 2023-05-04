fd4b3bc6394d4f78c0bf753974b455061da552b8
RDonii <r.donii@mail.ru>
2023-05-04 23:28:58 +0500

    djoser user serializer changed

--- medical_rec/settings.py
+++ medical_rec/settings.py
@@ -168,5 +168,6 @@ DJOSER = {
     'SERIALIZERS': {
         'user_create': 'core.serializers.UserCreateSerializer',
         'current_user': 'core.serializers.UserSerializer',
+        'user': 'core.serializers.UserSerializer',
     }
 }
