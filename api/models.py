772a5ba1b90c11d2bbf409cde0b91d1784e6550a
RDonii <r.donii@mail.ru>
2023-05-04 17:43:17 +0500

    Profile updated: company_name unique=False

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 
 class Profile(models.Model):
-    company_name = models.CharField(max_length=255, unique=True)
+    company_name = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
