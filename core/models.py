bdc08167f8a94616e38ff31d9306b42814a585da
RDonii <r.donii@mail.ru>
2023-05-04 22:48:21 +0500

    minor update: link added to comment

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 
 
-# Overriding default User model to avoid future migration conflicts
+# Overriding default User model to avoid future migration conflicts: https://docs.djangoproject.com/en/4.2/ref/settings/#std-setting-AUTH_USER_MODEL
 class User(AbstractUser):
     pass
