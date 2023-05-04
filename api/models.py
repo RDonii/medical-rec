b0a64eb856902dce4abb0a3a803adef4e2401f70
RDonii <r.donii@mail.ru>
2023-05-04 15:36:02 +0500

    Profile model added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    company_name = models.CharField(max_length=255, unique=True)
+    birth_date = models.DateField(null=True, blank=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['user__first_name', 'user__last_name']
