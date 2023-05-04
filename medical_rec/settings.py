f61d9a787e2262f918496215f2d8c45d8d7149a1
RDonii <r.donii@mail.ru>
2023-05-04 14:19:08 +0500

    starter configurations

--- medical_rec/settings.py
+++ medical_rec/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j4u61z9#sp92#=_@d8&1z#=lhx7_u9+gt6)a9%(5fo(l-rbf%n'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    env.str('HOST'),
+]
 
+if DEBUG:
+    ALLOWED_HOSTS += ['localhost', ]
 
 # Application definition
 
@@ -75,8 +83,12 @@ WSGI_APPLICATION = 'medical_rec.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('DB_NAME'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'USER': env.str('DB_USER'),
+        'PORT': env.str('DB_PORT'),
+        'HOST': env.str('DB_HOST'),
     }
 }
 
@@ -116,6 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static/'
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
