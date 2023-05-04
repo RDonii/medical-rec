34776cb4c526d35ee51ea6bd0479010416c84a43
RDonii <r.donii@mail.ru>
2023-05-04 16:00:39 +0500

    Patient model added

--- api/models.py
+++ api/models.py
@@ -9,3 +9,24 @@ class Profile(models.Model):
 
     class Meta:
         ordering = ['user__first_name', 'user__last_name']
+
+
+class Patient(models.Model):
+    class GenderChoice(models.TextChoices):
+        MALE = 'M'
+        FEMALE = 'F'
+
+    first_name = models.CharField(max_length=150, db_index=True)
+    last_name = models.CharField(max_length=150, db_index=True)
+    birth_date = models.DateField(null=True, blank=True, db_index=True)
+    gender = models.CharField(max_length=1, choices=GenderChoice.choices, null=True, blank=True)
+    med_condition = models.TextField()
+    doctor = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='patients')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = ['updated', 'first_name', 'last_name']
