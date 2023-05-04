90f466492c6e8e4127184ac78367e2e286b88c53
RDonii <r.donii@mail.ru>
2023-05-05 00:58:30 +0500

    code cleaning

--- core/tests/test_users.py
+++ core/tests/test_users.py
@@ -7,27 +7,47 @@ from model_bakery import baker
 import pytest
 
 
+@pytest.fixture
+def client():
+    return APIClient()
+
+@pytest.fixture
+def new_user_data():
+    return {
+        "username": "testusername",
+        "password": "testpassword123"
+    }
+
+@pytest.fixture
+def new_user(new_user_data):
+    return baker.make(settings.AUTH_USER_MODEL, **new_user_data)
+
+@pytest.fixture
+def new_staff_user(new_user_data):
+    return baker.make(settings.AUTH_USER_MODEL, **new_user_data, is_staff=True)
+
+@pytest.fixture
+def authenticated_client(client, new_user):
+    client.force_authenticate(new_user)
+    return client
+
+@pytest.fixture
+def authenticated_staff_client(client, new_staff_user):
+    client.force_authenticate(new_staff_user)
+    return client
+
+
 @pytest.mark.django_db
 class TestCreateUser:
     
-    def test_successful_user_registration_201(self):
-        client = APIClient()
-        user_data = {
-            "username": "u",
-            "password": "38p&dsjk1355*",
-            "first_name": "f",
-            "last_name": "l",
-            "email": "myemail@mail.ru"
-        }
-        
-        response = client.post(reverse('user-list'), user_data)
+    def test_successful_user_registration_201(self, client, new_user_data):
+        response = client.post(reverse('user-list'), new_user_data)
 
         assert response.data['id'] > 0
         assert "password" not in response.data.keys()
         assert response.status_code == status.HTTP_201_CREATED
 
-    def test_not_unique_username_registration_400(self):
-        client = APIClient()
+    def test_not_unique_username_registration_400(self, client):
         user1 = baker.make(settings.AUTH_USER_MODEL)
         user_data = {
             "username": user1.username,
@@ -43,15 +63,12 @@ class TestCreateUser:
 @pytest.mark.django_db
 class TestRetrieveUser:
 
-    def test_if_user_is_anonymous_returns_401(self):
-        client = APIClient()
-
+    def test_if_user_is_anonymous_returns_401(self, client):
         response = client.get(reverse('user-me'))
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_successful_retrieve_me(self):
-        client = APIClient()
+    def test_successful_retrieve_me(self, client):
         user = baker.make(settings.AUTH_USER_MODEL)
         client.force_authenticate(user=user)
 
@@ -60,8 +77,7 @@ class TestRetrieveUser:
         assert response.status_code == status.HTTP_200_OK
         assert response.data["id"] == user.id
 
-    def test_if_differant_id_returns_404(self):
-        client = APIClient()
+    def test_if_differant_id_returns_404(self, client):
         user = baker.make(settings.AUTH_USER_MODEL)
         client.force_authenticate(user=user)
 
@@ -73,31 +89,22 @@ class TestRetrieveUser:
 @pytest.mark.django_db
 class TestListUser:
 
-    def test_if_user_is_anonymous_returns_401(self):
-        client = APIClient()
-
+    def test_if_user_is_anonymous_returns_401(self, client):
         response = client.get(reverse('user-list'))
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
     
-    def test_if_user_is_not_staff_returns_self(self):
-        client = APIClient()
-        user = baker.make(settings.AUTH_USER_MODEL, is_staff=False)
-        client.force_authenticate(user=user)
-
-        response = client.get(reverse('user-list'))
+    def test_if_user_is_not_staff_returns_self(self, authenticated_client):
+        response = authenticated_client.get(reverse('user-list'))
 
         assert response.status_code == status.HTTP_200_OK
         assert isinstance(response.data, list)
         assert len(response.data) == 1
 
-    def test_if_user_is_staff_returns_all_users(self):
-        client = APIClient()
-        user = baker.make(settings.AUTH_USER_MODEL, is_staff=True)
-        client.force_authenticate(user=user)
+    def test_if_user_is_staff_returns_all_users(self, authenticated_staff_client):
         baker.make(settings.AUTH_USER_MODEL, __quantity=8)
 
-        response = client.get(reverse('user-list'))
+        response = authenticated_staff_client.get(reverse('user-list'))
 
         assert response.status_code == status.HTTP_200_OK
         assert isinstance(response.data, list)
