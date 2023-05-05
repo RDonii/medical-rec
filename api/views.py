0b1b4003fa4c4cb19d9bd810ee9546a5ac4fc5e3
RDonii <r.donii@mail.ru>
2023-05-05 11:52:59 +0500

    Profile viewset added

--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin, UpdateModelMixin
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
-# Create your views here.
+from api.models import Profile
+from api.serializers import ProfileSerializer
+
+
+class ProfileViewSet(ListModelMixin, UpdateModelMixin, GenericViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAdminUser]
+
+    @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated], url_path='me', url_name='me')
+    def get_profile(self, request):
+        serializer = self.get_serializer(request.user.profile)
+        return Response(serializer.data)
+
+    @get_profile.mapping.patch
+    def change_profile(self, request):
+        serializer = self.get_serializer(request.user.profile, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
