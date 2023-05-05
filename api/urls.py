e8e70fff41ebb0de4413235a13e2c5be8400f44b
RDonii <90790952+RDonii@users.noreply.github.com>
2023-05-05 22:34:46 +0500

    urls for material views

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,14 @@
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
 
-from api.views import ProfileViewSet, PatientViewSet
+from api.views import ProfileViewSet, PatientViewSet, MaterialViewSet
 
 
 router = DefaultRouter()
 router.register('profiles', ProfileViewSet)
 router.register('patients', PatientViewSet)
 
-urlpatterns = router.get_urls()
+patient_router = NestedDefaultRouter(router, 'patients', lookup='patient')
+patient_router.register('materials', MaterialViewSet)
+
+urlpatterns = router.urls + patient_router.urls
